aa5094bfd416b18dbd9ef690d29d9e220b9bd55e
Harry Greentree <harrygreentree@icloud.com>
2023-02-01 11:20:18 +0000

    Workshop 1 - Added triangleType.

--- Workshop1/src/io/github/theblacksquidward/Triangle.java
+++ Workshop1/src/io/github/theblacksquidward/Triangle.java
@@ -1,5 +1,10 @@
 package io.github.theblacksquidward;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Triangle {
 
     int a;
@@ -12,8 +17,24 @@ public class Triangle {
         this.c = c;
     }
 
+    String triangleType() {
+        List<Integer> ints = new ArrayList<>(List.of(a, b, c));
+        Collections.sort(ints);
+        int x = ints.get(0);
+        int y = ints.get(1);
+        int z = ints.get(2);
+
+        if(x == y && x == z) return "Equilateral";
+        else if(x*x + y*y == z*z) return "RightAngled";
+        else if(x == y || x == z || y == z) return "Isosceles";
+        else if(x + y == z) return "Flat";
+        else if(x + y < z) return "Impossible";
+        else return "Scalene";
+    }
+
     @Override
     public String toString() {
-        return "Triangle(" + a + ", " + b + ", " + c + ")";
+        return triangleType() + "(" + a + ", " + b + ", " + c + ")";
     }
+
 }
