16fa94fc079b81da877d58be86832f743d8684c4
Harry Greentree <harrygreentree@icloud.com>
2023-02-01 10:07:49 +0000

    Workshop 1 - 2a

---
+++ Workshop1/src/io/github/theblacksquidward/Critter.java
@@ -0,0 +1,15 @@
+package io.github.theblacksquidward;
+
+public class Critter {
+
+    private final String name;
+
+    public Critter(String name) {
+        this.name = name;
+    }
+
+    void poke() {
+        System.out.println("I was poked.");
+    }
+
+}
---
+++ Workshop1/src/io/github/theblacksquidward/Main.java
@@ -0,0 +1,10 @@
+package io.github.theblacksquidward;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Critter critter = new Critter("Cat");
+        critter.poke();
+    }
+
+}
