55308df1b94a14730f09712630e374c6b3451bd0
Harry Greentree <harrygreentree@icloud.com>
2023-02-01 10:56:32 +0000

    Workshop 1 - Added IntBoxTest, toString and add.

--- Workshop1/src/io/github/theblacksquidward/IntBox.java
+++ Workshop1/src/io/github/theblacksquidward/IntBox.java
@@ -1,9 +1,21 @@
 package io.github.theblacksquidward;
 
 public class IntBox {
+
     private int x;
 
     public IntBox(int x) {
         this.x = x;
     }
+
+    public IntBox add(int v) {
+        x+=v;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "IntBox(" + x + ")";
+    }
+
 }
---
+++ Workshop1/src/io/github/theblacksquidward/IntBoxTest.java
@@ -0,0 +1,10 @@
+package io.github.theblacksquidward;
+
+public class IntBoxTest {
+
+    public static void main(String[] args) {
+        IntBox a = new IntBox(10).add(10);
+        System.out.println(a);
+    }
+
+}
