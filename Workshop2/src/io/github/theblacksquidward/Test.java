540ec36104207c148ef922e8a2fec2b9d0cd4a73
Harry Greentree <harrygreentree@icloud.com>
2023-02-08 10:11:56 +0000

    Workshop 2 Task 1 - Part A.

--- Workshop2/src/io/github/theblacksquidward/Test.java
+++ Workshop2/src/io/github/theblacksquidward/Test.java
@@ -3,7 +3,17 @@ package io.github.theblacksquidward;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        final String prefix = "Error:";
+        try {
+            TestUtils.assertEquals("harry", "josh", prefix);
+        } catch (AssertionError e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            TestUtils.assertEquals(1, 2, prefix);
+        } catch (AssertionError e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
--- Workshop2/src/io/github/theblacksquidward/TestUtils.java
+++ Workshop2/src/io/github/theblacksquidward/TestUtils.java
@@ -3,11 +3,11 @@ package io.github.theblacksquidward;
 public class TestUtils {
 
     static void assertEquals(String expected, String actual, String prefix) {
-
+        if (!expected.equals(actual)) throw new AssertionError(prefix + " expecting " + expected + " but was " + actual);
     }
 
     static void assertEquals(int expected, int actual, String prefix) {
-        if (expected != actual) throw new AssertionError(prefix + ' expected ' + expected + ' got ' + actual);
+        if (expected != actual) throw new AssertionError(prefix + " expected " + expected + " got " + actual);
     }
 
 }
