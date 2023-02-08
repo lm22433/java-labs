a21b5b2bed9a14c7564164c82e2a2ccb4ec3d546
Harry Greentree <harrygreentree@icloud.com>
2023-02-08 10:02:10 +0000

    Workshop 2 - Added TestUtils.

---
+++ Workshop2/src/io/github/theblacksquidward/TestUtils.java
@@ -0,0 +1,13 @@
+package io.github.theblacksquidward;
+
+public class TestUtils {
+
+    static void assertEquals(String expected, String actual, String prefix) {
+
+    }
+
+    static void assertEquals(int expected, int actual, String prefix) {
+
+    }
+
+}
