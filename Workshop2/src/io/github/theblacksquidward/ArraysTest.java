ae43b92a1e087e48102c36db132a3a6339cf688c
josh chatten <dw22682@bristol.ac.uk>
2023-02-08 11:13:59 +0000

    Array task

---
+++ Workshop2/src/io/github/theblacksquidward/ArraysTest.java
@@ -0,0 +1,33 @@
+package io.github.theblacksquidward;
+
+public class ArraysTest {
+
+    public static void main(String[] args) {
+        int[] xs = new int[] {1,2,6,5};
+        int[] ys = new int[] {};
+
+        try {
+            System.out.println(ArraysTest.max(null));
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+        try {
+            System.out.println(ArraysTest.max(ys));
+        }
+        catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+        System.out.println(ArraysTest.max(xs));
+    }
+
+    public static int max(int[] xs) {
+        if (xs == null) throw new IllegalArgumentException();
+        if (xs.length == 0) throw new IllegalArgumentException();
+        int maxval = 0;
+        for (int i = 0; i < xs.length; i++) {
+            if (xs[i] > maxval) maxval = xs[i];
+        }
+        return maxval;
+    }
+}
