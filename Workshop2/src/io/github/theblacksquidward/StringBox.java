0c50dc089fef4e0ac859ab11f6d638fab4401187
Harry Greentree <harrygreentree@icloud.com>
2023-02-08 10:23:56 +0000

    Workshop 2 - Added StringUtils and StringBox.

---
+++ Workshop2/src/io/github/theblacksquidward/StringBox.java
@@ -0,0 +1,15 @@
+package io.github.theblacksquidward;
+
+public class StringBox {
+
+    String value;
+
+    public StringBox(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
---
+++ Workshop2/src/io/github/theblacksquidward/StringUtils.java
@@ -0,0 +1,20 @@
+package io.github.theblacksquidward;
+
+public class StringUtils {
+
+    static void append(String a) {
+
+    }
+
+    static String append2(final String a) {
+        return null;
+    }
+
+    static void concat(StringBox a) {
+
+    }
+
+
+
+
+}
