dececa684bbb0ff03b2554ef7c0c78d0c397eb8a
Harry Greentree <harrygreentree@icloud.com>
2023-02-15 10:06:35 +0000

    Added reverse.

--- Workshop3/src/io/github/theblacksquidward/Main.java
+++ Workshop3/src/io/github/theblacksquidward/Main.java
@@ -1,16 +1,28 @@
 package io.github.theblacksquidward;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        return;
+        List<String> names = List.of("Harry", "Josh", "Lewis", "Fin", "Bagga");
+        reverse(names).forEach(System.out::println);
     }
 
     static boolean isEqual(Object left, Object right) {
         return left.equals(right);
     }
 
-    static <t> boolean isEqual2(t left, t right) {
+    static <T> boolean isEqual2(T left, T right) {
         return left.equals(right);
     }
+
+    static <T> List<T> reverse(List<T> input) {
+        List<T> list = new ArrayList<>(input);
+        Collections.reverse(list);
+        return list;
+    }
+
 }
