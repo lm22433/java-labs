2d5d33a7a01e8af241bd8d1c9b717d784a7e8d9a
Harry Greentree <harrygreentree@icloud.com>
2023-02-15 10:38:41 +0000

    Added maybe.

--- Workshop3/src/io/github/theblacksquidward/Maybe.java
+++ Workshop3/src/io/github/theblacksquidward/Maybe.java
@@ -1,9 +1,42 @@
 package io.github.theblacksquidward;
 
-public class Maybe {
+import java.util.function.Function;
+import java.util.function.Supplier;
 
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class Maybe<T> {
+
+    final T value;
+
+    private Maybe(T value) {
+        this.value = value;
+    }
+
+    public static <T> Maybe<T> nothing() {
+        return new Maybe<>(null);
+    }
+
+    public static <T> Maybe<T> just(T value) {
+        return new Maybe<>(value);
+    }
+
+    boolean isEmpty() {
+        return value == null;
+    }
+
+    public <U> Maybe<U> map(Function<T, U> function) {
+        return isEmpty() ? nothing() : Maybe.just(function.apply(value));
+    }
+
+    public <U> Maybe<U> flatMap(Function<T, Maybe<U>> function) {
+        return isEmpty() ? nothing() : function.apply(value);
+    }
+
+    public <U> U fold(Function<T, U> function, Supplier<U> supplier) {
+        return isEmpty() ? supplier.get() : function.apply(value);
+    }
+
+    public T get() {
+        return value;
     }
 
 }
