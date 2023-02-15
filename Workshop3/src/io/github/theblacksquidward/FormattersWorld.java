9c9a36680387299478727d8e205783234d14d4de
josh chatten <dw22682@bristol.ac.uk>
2023-02-15 11:13:32 +0000

    added rot13

--- Workshop3/src/io/github/theblacksquidward/FormattersWorld.java
+++ Workshop3/src/io/github/theblacksquidward/FormattersWorld.java
@@ -11,6 +11,19 @@ public class FormattersWorld {
                 .forEach(System.out::println);
     }
 
+    public static String rot13(String input) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if       (c >= 'a' && c <= 'm') c += 13;
+            else if  (c >= 'A' && c <= 'M') c += 13;
+            else if  (c >= 'n' && c <= 'z') c -= 13;
+            else if  (c >= 'N' && c <= 'Z') c -= 13;
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+
     interface Formatter {
         String format(String input);
     }
@@ -26,6 +39,14 @@ public class FormattersWorld {
         @Override
         public String format(String input) {
             return input.toUpperCase();
+        }
+    }
+
+    static class ROT13Formatter implements Formatter {
+
+        @Override
+        public String format(String input) {
+            return rot13(input);
         }
     }
 
